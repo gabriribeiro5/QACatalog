734efb5070ba178fdded04da07769d87c94bd37c
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2022-03-07 23:03:49 -0300

    feat(findQuestions): method is ready to go

--- tagManager.py
+++ tagManager.py
@@ -1,42 +1,52 @@
+from tomlkit import value
 import fileManager
 import logSet
 import logging
+import datetime
+
+logSet.enableLog()
 
 def inputTags(question=dict, tags=list, overwritte:bool=False):
-    logging.info(" #### iniciating inputTags ###")
-    questionsAndTags = fileManager.loadDataFrom("./questionsAndTags.yaml")
-    logging.info("previous questionsAndTags:")
-    logging.info(questionsAndTags)
+    now = datetime.datetime.today()
+    logging.info(f" \n {now} \n #### iniciating inputTags ####")
+    logging.warning(f"Overwritte mode: {overwritte} \n")
 
-    # cases to ignore existing values
+    questionsAndTags = fileManager.loadDataFrom("./questionsAndTags.yaml")
+
+    # Cases to ignore existing values
     if question not in questionsAndTags or overwritte:
+        logging.info(f" {now} \n adding new values or overwritting existing ones")
         questionsAndTags[question] = tags
-    #Add only new tags if question alerady exists
+    # Add only new tags if question alerady exists
     else:
+        logging.info("appending new values only")
         qt = questionsAndTags[question]
         for t in tags:
             if t in qt:
                 pass
             else:
                 questionsAndTags[question].append(t)
-        
-
     
-    logging.info("final questionsAndTags:")
-    logging.info(questionsAndTags)
-
     fileManager.updateFile(questionsAndTags, "./questionsAndTags.yaml")
-
-    questionsAndTags = fileManager.loadDataFrom("./questionsAndTags.yaml")
-    logging.info("file looks like:")
-    logging.info(questionsAndTags)
-
+    logging.info("file updated")
     return questionsAndTags
 
-inputTags(question="q54", tags=["Yeh! Science bitch!"])
+inputTags(question="q54", tags=["Yeh! Science bitch!"], overwritte=True)
 
 
+def findQuestions(tags={}):
+    now = datetime.datetime.today()
+    logging.info(f" \n {now} \n #### searching questions by tags ####")
 
+    questionsAndTags = fileManager.loadDataFrom("./questionsAndTags.yaml")
+    questionsList:dict = {}
 
-# def findQuestions(tags={}):
-#     a = 
+    for t in tags:
+        for k, v in questionsAndTags.items():
+            if t in v:
+                questionsList[k] = v
+    
+    return questionsList
+
+questionsList = findQuestions(tags=["Lambda", "CloudWatch"])
+print(f"questionsList: {questionsList}")
