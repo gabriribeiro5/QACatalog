b25566beec161c64aec36e59ff6880164fa24db2
Gabriel Ribeiro Emegildio <gribeire@emeal.nttdata.com>
2022-03-18 18:42:56 -0300

    test(tests): tests for logging module

---
+++ tests/logTester.py
@@ -0,0 +1,17 @@
+# Dealing with ImportError: attempted relative import with no known parent package
+import sys
+sys.path.append('.')
+
+from scripts.log import logSet
+import logging
+
+logSet.enableLog("tests", "logTester")
+
+logging.debug('logTester starts here')
+logging.info('This message should go to the log file')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+anyVar = 'xablau'
+anotherVar = 'bazinga'
+logging.info('Variable 1 (%s) and variable 2 (%s) come from python code', anyVar, anotherVar)
