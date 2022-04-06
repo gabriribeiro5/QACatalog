ab47208c31448db7fc0ac888dc8254edff5036ca
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2022-04-06 06:13:27 -0300

    refactor(input-handler->-talkToUser.py): move input handler from main.py to new module talkToUser

--- main.py
+++ main.py
@@ -1,42 +1,8 @@
-import tagManager
-import fileManager
+from user_interface import talkToUser
 import logSet
 import logging
-import datetime
 
-logSet.enableLog()
+logSet.enableLog(".", "tagmanager2")
+logging.info(' ++++++++++++++++++++++++++++++++++ tagmanager initiated ++++++++++++++++++++++++++++++++++')
 
-answer = input("### MENU ### \n 1. input \n 2. search by tag: \n >>")
-
-# input handler
-if answer == "1":
-    i = input(">> Enter question number: ")
-    q = f"q{i}"
-    questionsAndTags = fileManager.loadDataFrom("./questionsAndTags.yaml")
-    # dealing with existing questions
-    if q in questionsAndTags:
-        questionValue = questionsAndTags.items(q)
-        print(f">> Question {q}: {questionValue}")
-        i = input("OVERWRITTE current values (Y/n)? ")
-        # OVERWRITE MODE ON
-        if i == "Y" or i == "y":
-            i = input("ATENTION: All current values will be lost if you proceed. Proceed(Y/n)? ")
-            if i == "Y" or i == "y":
-                t = input(f"TAGS for question {q} (separeted, by, comma): ")
-                tagManager.inputTags(question=q, tags=t, overwritte=True)
-        # OVERWRITE MODE OFF
-        else:
-            t = input(f"TAGS for question {q} (separeted, by, comma): \n >>")
-            tagManager.inputTags(question=q, tags=t)
-    # dealing with new questions
-    else:
-        i = input("Question number not found. \n 1. Continue to add new question \n 2. Enter new question number \n >>")
-        if i == "1":
-            t = input(f"TAGS for question {q} (separeted, by, comma): \n >>")
-            tagManager.inputTags(question=q, tags=t)
-
-
-        # tagManager.inputTags(question=q, tags=t)
-
-    i = input("Enter, tags, separeted, by coma:")
-    # tagManager.inputTags(question=q, tags=t)
+talkToUser.start_talk()
---
+++ user_interface/talkToUser.py
@@ -0,0 +1,50 @@
+import tagManager
+import utils.fileManager as fileManager
+import logging
+
+class start_talk(object):
+    logging.info("Starting talk to user")
+
+    def mainMenu(self):
+        menu = "\n ### MAIN MENU ### \n What task do you wish to perform? \n 1 = input new data \n 2 = search existing data by tag: \n Enter a number: "
+        answer = input(menu)
+        return answer
+    
+    def inputHandler(self):
+        answer = self.mainMenu()
+        if answer == "1":
+            logging.debug("MAIN MENU: user has entered option 1")
+
+            i = input(">> Enter question number: ")
+            q = f"q{i}"
+            questionsAndTags = fileManager.loadDataFrom("./questionsAndTags.yaml")
+            # dealing with existing questions
+            if q in questionsAndTags:
+                questionValue = questionsAndTags.items(q)
+                print("Current values:")
+                print(f">> Question {q}: {questionValue}")
+                i = input("OVERWRITTE current values (Y/n)? ")
+                # OVERWRITE MODE ON
+                if i == "Y" or i == "y":
+                    i = input("ATENTION: All current values will be lost. Press Enter to cancel or insert 'go' to proceed: ")
+                    if i == "Y" or i == "y":
+                        t = input(f"enter TAGS for question {q} (separeted, by, comma): ")
+                        tagManager.inputTags(question=q, tags=t, overwritte=True)
+                    else:
+                        pass
+                # OVERWRITE MODE OFF
+                else:
+                    t = input(f"TAGS for question {q} (separeted, by, comma): \n >>")
+                    tagManager.inputTags(question=q, tags=t)
+            # dealing with new questions
+            else:
+                i = input("Question number not found. \n 1. Continue to add new question \n 2. Enter new question number \n >>")
+                if i == "1":
+                    t = input(f"TAGS for question {q} (separeted, by, comma): \n >>")
+                    tagManager.inputTags(question=q, tags=t)
+
+
+                # tagManager.inputTags(question=q, tags=t)
+
+            i = input("Enter, tags, separeted, by coma:")
+            # tagManager.inputTags(question=q, tags=t)
