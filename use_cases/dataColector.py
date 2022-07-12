5083dd94492aaaead1f20c340a5af72c92be7ce1
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2022-07-12 17:15:16 -0300

    perf(dataColector): openPDF operations are now executed on __init__ and global variables are available along every class function

--- use_cases/dataColector.py
+++ use_cases/dataColector.py
@@ -1,4 +1,5 @@
 # Dealing with ImportError: attempted relative import with no known parent package
+from imghdr import tests
 import pdb
 import sys
 sys.path.append('.')
@@ -7,32 +8,29 @@ import fitz #PyMuPDF
 import logging
 
 class PDF_Master(object):
-    def __init__(self):
-        pass
-    
-    def openPDF(self, fileName:str=None, dirName:str="data"):
+    def __init__(self, fileName:str=None, dirName:str="data"):
+        """
+        fileName: give me a PDF name and I will handle it
+        dirName: primary source directory is /data, any other dir must be specified
+        """
+        logging.info("Starting class PDF Master")
+
         if fileName is None:
             response = "no file specified"
             return response
-        
+
         if ".pdf" in fileName:
             fileName = fileName.split(".")[0]
 
-        pdf_file = f"{dirName}/{fileName}.pdf"
+        self.filePathName = f"{dirName}/{fileName}.pdf"
+        self.doc = fitz.open(self.filePathName)
+        
 
-        doc = fitz.open(pdf_file)
-        print(type(doc.pages()))
-        return doc
-
-    def getQuestions(self, fileName:str=None, dirName:str="data"):
-        doc = self.openPDF(fileName)
-
+    def getQuestions(self):
         allQuestions = []
         count = 0
-        print(type(doc.pages()))
-        for page in doc.pages():
-            if count == 0:
-                print(type(page))
+        
+        for page in self.doc.pages():
             blocks = page.get_text("blocks") # all paragraphs
             copy = False
             question = []
@@ -51,15 +49,40 @@ class PDF_Master(object):
                 copy = False
                 if refStart in str(paragraph[4]):
                     count += 1
-                    question = f'{count}. Question'
+                    question = f'{paragraph[4][-13]}. Question:' # "-13" refers to the character where the question number is located
                     copy = True
                 else:
                     copy = False
-        doc.close()    
+        self.doc.close()    
         return allQuestions
-                
-                
 
-a = PDF_Master()
-b = [a.getQuestions("AWS_CDA_Practice+Questions_DCT_2021")]
-# print(b)
+    def totalPages(self):
+        value = len(self.doc)
+        return value
+
+    def newFile(self, newFilePathName:str=None, copyFromPage=0, copyToPage=0):
+        """
+        newFilePathName = "path/to/fileName.pdf",
+        copyFromPage = initial page to copy from source,
+        copyToPage = last page to copy from source
+        """
+        if ".pdf" in self.filePathName:
+            self.filePathName = self.filePathName.split(".")[0]
+
+        if newFilePathName is None:
+            newFilePathName = f"{self.filePathName}_newFile.pdf"
+
+        newDoc = fitz.open()                 # new empty PDF
+        newDoc.insert_pdf(self.doc,from_page = copyFromPage, to_page = copyToPage)  # first 10 pages
+        logging.info(f"saving new PDF: {newFilePathName}")
+        newDoc.save(f"{newFilePathName}")
+        
+
+simAWS = PDF_Master("source_test_dataColector", "tests")
+q = simAWS.getQuestions()
+print(q[0])
+
+# print(PDF_Master.__getattribute__)
+# print(simAWS.__init__.__code__)
+# print(PDF_Master.doc)
+# simAWS.newFile("tests/source_test_dataColector.pdf", 4, 7)
